b588b12261f9dfb1d44836aa803330fc09f011f6
samelie <sam@stinkdigital.com>
2018-06-04 23:23:17 -0700

    no destrcution

--- install.sh
+++ install.sh
@@ -5,14 +5,8 @@
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 # nvm install 9.4.0
 # nvm alias default 9.4.0
-npm i pm2 -g
 rm -rf public/static/4afc
 git clone https://github.com/enchroma/4afc.git public/static/4afc
-cd public/static/4afc
-npm install
-npm run build:prod
-rm -rf node_modules
-cd ../../../
 npm i
 npm run build
 ./start.sh

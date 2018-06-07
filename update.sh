3226d55b4c2824015dd614b643c03abab01a83b5
samelie <sam@stinkdigital.com>
2018-06-06 18:17:13 -0700

    updates.h

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+rm -rf public/static/4afc
+rm -rf public/static/after-image-v1
+git clone https://github.com/enchroma/4afc.git public/static/4afc
+git clone https://github.com/enchroma/after-image-v1.git public/static/after-image-v1
+
+
+

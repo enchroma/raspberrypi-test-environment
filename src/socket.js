f1c90099aa11d35ab6a5c2965dd2ff5e45cb4100
samelie <sam@stinkdigital.com>
2018-06-21 20:56:58 -0700

    ignore faulty values

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,9 @@ import io from "socket.io-client"
 
 class Socket {
   constructor() {
+    /*************
+     *  https://github.com/enchroma/after-image-v1
+     ************ */
     const rSliderEl = document.querySelector(".slider--red")
     const gSliderEl = document.querySelector(".slider--green")
     const bSliderEl = document.querySelector(".slider--blue")
@@ -12,14 +15,26 @@ class Socket {
       bSliderEl.noUiSlider,
     ]
 
+    const values = [[], [], []]
+
     this._socket = io(window.SOCKET_URL)
 
     this._socket.on("color", data => {
       const c = data.split(" ")
       for (var i = 0; i < data.length; i++) {
-        if(noUiSliders[i]){
-          const v1 = parseInt(c[i] / 1023 * 255);
-          noUiSliders[i].set([v1])
+        if (noUiSliders[i]) {
+          const v1 = parseInt((c[i] / 1023) * 255)
+          values[i].push(v1)
+          const l = values[i].length
+          if (l > 2) {
+            //ignore
+            if (values[i][l - 3] !== v1) {
+              noUiSliders[i].set([v1])
+            }
+            values[i].shift()
+          } else {
+            noUiSliders[i].set([v1])
+          }
         }
       }
     })
